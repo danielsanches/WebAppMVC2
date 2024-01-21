6a0de212efebcd6672af8341c73cf990a42e626d
Daniel <Daniel@DJANGO-LIVRE-PC>
2024-01-21 16:21:35 -0400

    Add project files.

---
+++ Models/ItensGrid.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppMVC2.Models
+{
+    public class ItensGrid
+    {
+        public int Id { get; set; }
+        public int Quantidade { get; set; }
+        public string Nome { get; set; }
+    }
+}
